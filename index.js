194db467e5ac97a6a47849493badbf77b7fa3fcf
Igoche John Eigege <140503808+Melchizedeke@users.noreply.github.com>
2024-04-25 11:16:33 +0100

    modified the js file

--- index.js
+++
@@ -1,9 +0,0 @@
-// a function to copy the text from the input form
-function copyText() {
-    let copyText = document.querySelector("input");
-    copyText.select();
-    document.execCommand("copy");
-    document.body.appendChild(copyText.value);
-    alert(copyText.value);
-}
-
