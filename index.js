8693a804705e87ac3dd4f21ae187cedddf8fed3a
Igoche John Eigege <140503808+Melchizedeke@users.noreply.github.com>
2024-04-25 12:11:41 +0100

    added javasxript codes

---
+++ index.js
@@ -0,0 +1,17 @@
+//Adding validation for an empty field
+document.querySelector('#add-todo').onclick = function () {
+    if (document.querySelector('#newtask input').value.length === 0) {
+        alert('Please enter a valid todo item.')
+    }
+
+    //Adding a new task    
+    else {
+        document.querySelector('#tasks').innerHTML +=
+            `<div class='task'>
+            <span id="taskname">
+                ${document.querySelector('#newtask input').value}
+            </span>
+        </div>`
+    }
+
+}
